f9093a85d4074f0d336942b601cc2011f3675e33
Huyen Trang Dinh <tran9dinh@gmail.com>
2018-10-06 15:12:43 -0400

    pls

--- src/main/java/io/polyhx/lhgames/game/bot/Bot.java
+++ src/main/java/io/polyhx/lhgames/game/bot/Bot.java
@@ -11,17 +11,17 @@ import java.util.List;
 public class Bot extends BaseBot {
 	private int i = 0; 
     public IAction getAction(Map map, Player player, List<Player> others, GameInfo info) {
-    	if (i <= 9) {
+    	if (i < 2) {
     		i++;
-    		return createMoveAction(Point.LEFT);
+    		return createMoveAction(Point.DOWN);
     	}
     	else if ( i  <=  12) {
     		i++;
-    		return createMoveAction(Point.UP);
+    		return createMoveAction(Point.RIGHT);
     	}
     	
     	else {
-    		return null;
+    		return createCollectAction(Point.RIGHT);
     	}
 
     } 
