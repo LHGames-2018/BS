e4d94a9a68cda42de2ffabda2cc31c0ab6867d12
Jerry <jerrryyl@gmail.com>
2018-10-06 12:19:13 -0400

    Division des taches

--- src/main/java/io/polyhx/lhgames/game/bot/Bot.java
+++ src/main/java/io/polyhx/lhgames/game/bot/Bot.java
@@ -9,7 +9,42 @@ import io.polyhx.lhgames.game.point.Point;
 import java.util.List;
 
 public class Bot extends BaseBot {
-    public IAction getAction(Map map, Player player, List<Player> others, GameInfo info) {
-        return createMoveAction(Point.RIGHT);
-    }
+
+	// Etats possibles, ajoutez-en tant que vous voulez.
+	private enum State {RUN_STRAIGHT, ATTACK, MOVE, GATHER, HOME, FLEE};
+
+	// L'etat principal du robot
+	private State mainState = RUN_STRAIGHT;
+
+	public IAction getAction(Map map, Player player, List<Player> others, GameInfo info) {
+		switch (mainState) {
+		case RUN_STRAIGHT: {
+			return getRunStraightAction();
+		}
+		case ATTACK: {
+			return getRunStraightAction();
+		}
+		case MOVE: {
+			return getRunStraightAction();
+		}
+		case GATHER: {
+			return getRunStraightAction();
+		}
+		case HOME: {
+			return getRunStraightAction();
+		}
+		case FLEE: {
+			return getRunStraightAction();
+		}
+		default : {
+			return null;
+		}
+		}
+	}
+	
+	// Courrir tout droit
+	public IAction getRunStraightAction() {
+		
+	}
+	
 }
